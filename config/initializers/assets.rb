8c1e8f5243bd7db24b51ce17411843267f8528fa
David Bennett <david.bennett@ncf.edu>
2017-04-22 17:17:26 -0400

    commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css 1-col-portfo
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
