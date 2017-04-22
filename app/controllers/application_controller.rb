6a325c013edd072c23c837dc1f1102f582967563
David Bennett <david.bennett@ncf.edu>
2017-04-22 13:39:48 -0400

    basic devise functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+protect_from_forgery with: :exception
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:name, :location, :age, :occupation)
     end
   end
+  #
+  # def resource_name
+  #   :user
+  # end
+  #
+  # def resource
+  #   @resource ||= User.new
+  # end
+  #
+  # def devise_mapping
+  #   @devise_mapping ||= Devise.mappings[:user]
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :events
   resources :categories
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
-  
+  resources :users
 
 end
